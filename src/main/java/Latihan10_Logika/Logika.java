f2f4176e37a5c701ce074f06c7c827988c7029a7
afifarn <afifr0837@gmail.com>
2019-09-25 15:47:14 +0700

    first commit

---
+++ src/main/java/Latihan10_Logika/Logika.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan10_Logika;
+
+/**
+ *
+ * @author 
+ * NAMA     : Afif Raditya Nugraha
+ * KELAS    : PBO_ULANG/IF4
+ * NIM      : 10116138
+ * LOG      : Program ini berisi program untuk menampilkan logika umur
+ */
+public class Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >=17);
+        System.out.println("Status Dewasa :" + dewasa);
+    }
+    
+}
